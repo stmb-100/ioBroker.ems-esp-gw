1dd6ae0484a708faa9144f908c9a70e50a7d9ced
Stefan Mühlbauer <github@stmb-mail.de>
2021-05-14 09:15:13 +0200

    12. commit

--- admin/words.js
+++ admin/words.js
@@ -4,110 +4,59 @@
 systemDictionary = {
 	'ems-esp-gw adapter settings': {
 		'en': 'Adapter settings for ems-esp-gw',
-		'de': 'Adaptereinstellungen für ems-esp-gw',
-		'ru': 'Настройки адаптера для ems-esp-gw',
-		'pt': 'Configurações do adaptador para ems-esp-gw',
-		'nl': 'Adapterinstellingen voor ems-esp-gw',
-		'fr': "Paramètres d'adaptateur pour ems-esp-gw",
-		'it': "Impostazioni dell'adattatore per ems-esp-gw",
-		'es': 'Ajustes del adaptador para ems-esp-gw',
-		'pl': 'Ustawienia adaptera dla ems-esp-gw',
-		'zh-cn': 'ems-esp-gw的适配器设置'
+		'de': 'Adaptereinstellungen für ems-esp-gw'
+
 	},
-	'option1': {
-		'en': 'option1',
-		'de': 'Option 1',
-		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'
+	'System Info': {
+		'en': 'System Info',
+		'de': 'System Info'
+	},		
+	'Dallas Sensor Info': {
+		'en': 'Dallas Sensor Info',
+		'de': 'Dallas Sensor Info'
 	},
-	'option2': {
-		'en': 'option2',
-		'de': 'Option 2',
-		'ru': 'option2',
-		'pt': 'opção 2',
-		'nl': 'Optie 2',
-		'fr': 'Option 2',
-		'it': 'opzione 2',
-		'es': 'opcion 2',
-		'pl': 'Opcja 2',
-		'zh-cn': '选项2'
+	'Solar Info': {
+		'en': 'Solar Info',
+		'de': 'Solar Info'
+	},
+	'Thermostat Info': {
+		'en': 'Thermostat Info',
+		'de': 'Thermostat Info'
+	},
+	'TBoiler Info': {
+		'en': 'Boiler Info',
+		'de': 'Boiler Info'
+	},
+	'EMS-ESP-IPAdresse': {
+		'en': 'EMS-ESP-IPAdress',
+		'de': 'EMS-ESP-IPAdresse'
+	},
+	'EMS-ESP-IPPort': {
+		'en': 'EMS-ESP-IPPort',
+		'de': 'EMS-ESP-IPPort'
 	},
 	'myColor': {
 		'en': 'myColor',
-		'de': 'meineColor',
-		'ru': 'Мой цвет',
-		'pt': 'minhaCor',
-		'nl': 'mijnKleur',
-		'fr': 'maCouleur',
-		'it': 'mioColore',
-		'es': 'miColor',
-		'pl': 'mójKolor',
-		'zh-cn': '我的颜色'
+		'de': 'meineColor'
 	},
 	'myColor_tooltip': {
 		'en': 'Description of\nmyColor',
-		'de': 'Beschreibung von\nmyColor',
-		'ru': 'Описание\nmyColor',
-		'pt': 'Descrição de\nmyColor',
-		'nl': 'Beschrijving van\nmyColor',
-		'fr': 'Description de\nmyColor',
-		'it': 'Descrizione di\nmyColor',
-		'es': 'Descripción de\nmyColor',
-		'pl': 'Opis\nmyColor',
-		'zh-cn': '\nmyColor的描述'
+		'de': 'Beschreibung von\nmyColor'
 	},
 	'htmlText': {
 		'en': 'htmlText',
-		'de': 'htmlText',
-		'ru': 'htmlText',
-		'pt': 'htmlText',
-		'nl': 'htmlText',
-		'fr': 'htmlText',
-		'it': 'htmlText',
-		'es': 'htmlText',
-		'pl': 'htmlText',
-		'zh-cn': 'htmlText'
+		'de': 'htmlText'
 	},
 	'group_extraMyset': {
 		'en': 'extraMyset',
-		'de': 'extraMyset',
-		'ru': 'extraMyset',
-		'pt': 'extraMyset',
-		'nl': 'extraMyset',
-		'fr': 'extraMyset',
-		'it': 'extraMyset',
-		'es': 'extraMyset',
-		'pl': 'extraMyset',
-		'zh-cn': 'extraMyset'
+		'de': 'extraMyset'
 	},
 	'extraAttr': {
 		'en': 'extraAttr',
-		'de': 'extraAttr',
-		'ru': 'extraAttr',
-		'pt': 'extraAttr',
-		'nl': 'extraAttr',
-		'fr': 'extraAttr',
-		'it': 'extraAttr',
-		'es': 'extraAttr',
-		'pl': 'extraAttr',
-		'zh-cn': 'extraAttr'
+		'de': 'extraAttr'
 	},
 	'Instance': {
 		'en': 'Instance',
-		'de': 'Instanz',
-		'ru': 'Инстанция',
-		'pt': 'Instância',
-		'nl': 'Instantie',
-		'fr': 'Instance',
-		'it': 'Esempio',
-		'es': 'Instancia',
-		'pl': 'Instancja',
-		'zh-cn': '例'
+		'de': 'Instanz'
 	}
 };
